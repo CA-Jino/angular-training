35142947db2325d348c59750c94ae562e5e44b02
CA-Jino <kim@cloud-ace.jp>
2021-01-29 22:35:41 +0900

    04-12. メッセージログを表示する

--- admin-app/src/app/member.service.ts
+++ admin-app/src/app/member.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Member } from './member';
 import { MEMBERS } from './mock-members';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getMembers(): Observable<Member[]> {
+    this.messageService.add('MemberService: 사원 일람 데이터를 취득했습니다.');
     return of(MEMBERS);
   }
 }
