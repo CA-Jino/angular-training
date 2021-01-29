4437c1c6ef9ba92aee4f8b16362e1e1461c1a219
CA-Jino <kim@cloud-ace.jp>
2021-01-29 23:24:07 +0900

    04-14. ルーティングを設定する

--- admin-app/src/app/app-routing.module.ts
+++ admin-app/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MembersComponent } from './members/members.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'members', component: MembersComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
