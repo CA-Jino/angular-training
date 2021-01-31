96dd07cc09d756f8318e6cefa78fd9e3d020269a
CA-Jino <kim@cloud-ace.jp>
2021-01-31 23:07:15 +0900

    4-15. 詳細ページへのリンクを設定する

--- admin-app/src/app/app-routing.module.ts
+++ admin-app/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MembersComponent } from './members/members.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'members', component: MembersComponent }
 ];
 
