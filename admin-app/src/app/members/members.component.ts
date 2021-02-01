cab412d7546a00f60c385680b2cf4909ec551039
CA-Jino <kim@cloud-ace.jp>
2021-02-01 09:19:31 +0900

    4-16. 詳細ページへのリンクを設定する

--- admin-app/src/app/members/members.component.ts
+++ admin-app/src/app/members/members.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member';
 import { MemberService } from '../member.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-members',
@@ -11,21 +10,12 @@ import { MessageService } from '../message.service';
 export class MembersComponent implements OnInit {
 
   members: Member[];
-  selectedMember: Member;
 
-  constructor(
-    private memberService: MemberService,
-    private messageService: MessageService
-  ) { }
+  constructor(private memberService: MemberService) { }
 
   // life cycle method；コンポーネントが初期化される際に実行されるメソッド
   ngOnInit(): void {
     this.getMembers();
-  }
-
-  onSelect(member: Member): void {
-    this.selectedMember = member;
-    this.messageService.add(`MembersComponent: 사원데이터(id=${member.id})가 선택되었습니다.`);
   }
 
   getMembers(): void {
